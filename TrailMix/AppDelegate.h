049d8fd59e2bf03767c5def21c7be24a09f6eab8
Henry Chan <henry.d.chan@gmail.com>
2015-07-29 16:05:28 -0400

    Initial commit

---
+++ TrailMix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrailMix
+//
+//  Created by Henry Chan on 7/29/15.
+//  Copyright (c) 2015 Team Fax Machine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrailMix/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TrailMix
+//
+//  Created by Henry Chan on 7/29/15.
+//  Copyright (c) 2015 Team Fax Machine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
